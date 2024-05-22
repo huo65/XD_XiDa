0ed3cded3db52b93a41b3163be46cb85d984bc21
huo65 <1599272181@qq.com>
2024-05-22 17:39:43 +0800

    配置文件

---
+++ xida-backend/src/main/java/net/eight/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package net.eight.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis Plus配置
+ *
+ * @author  
+ * @date 2023/07/28
+ */
+@Configuration
+@MapperScan("net.eight.mapper")
+public class MybatisPlusConfig {
+    /**
+     * MyBatis Plus拦截器
+     *
+     * @return {@link MybatisPlusInterceptor}
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        //新建分页插件
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
