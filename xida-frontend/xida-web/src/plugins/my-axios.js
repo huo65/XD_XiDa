986aeb926dc623653e38856681981136a7cced0d
rainowyy <caoruoyu321@163.com>
2024-05-24 14:47:27 +0800

    添加响应拦截器

--- xida-frontend/xida-web/src/plugins/my-axios.js
+++ xida-frontend/xida-web/src/plugins/my-axios.js
@@ -19,4 +19,19 @@ myAxios.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
+// 添加响应拦截器
+myAxios.interceptors.response.use(function (response) {
+    if (response?.data?.code === 40100) {
+        showFailToast("未登录")
+        const redirectUrl = window.location.href
+        window.location.href = `/user/login?redirect=${redirectUrl}`
+    }
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response;
+}, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
 export default myAxios
