7dfb4d23352179361529fe524b72297f0ba3802e
rainowyy <caoruoyu321@163.com>
2024-06-03 22:34:09 +0800

    管理端前端-整体布局

---
+++ xida-frontend/xida-admin/src/components/Home.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import TopBar from "./TopBar.vue";
+import SideBar from "./SideBar.vue";
+
+</script>
+
+<template>
+  <el-container class="home-container">
+    <el-header>
+      <TopBar/>
+    </el-header>
+    <el-container>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+.el-main{
+  background-color: #eaedf1;
+}
+.home-container{
+  height: 100%;
+}
+</style>
