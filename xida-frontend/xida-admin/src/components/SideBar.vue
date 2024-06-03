4921aa823bd2bb83eb3459147c6d2ff71bc0fab2
rainowyy <caoruoyu321@163.com>
2024-06-03 22:33:27 +0800

    管理端前端-侧边栏

---
+++ xida-frontend/xida-admin/src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-menu
+      :default-active="activeP"
+      style="height: 100%"
+      router
+  >
+    <el-menu-item index="dashboard" @click="saveNavState('dashboard')">
+      <el-icon>
+        <location/>
+      </el-icon>
+      <span>工作台</span>
+    </el-menu-item>
+    <el-menu-item index="user" @click="saveNavState('user')">
+      <el-icon>
+        <User/>
+      </el-icon>
+      <span>用户管理</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import {Location, User} from "@element-plus/icons-vue";
+import {onMounted, ref} from "vue";
+
+const activeP = ref("")
+
+onMounted(() => {
+  activeP.value = String(sessionStorage.getItem('activePath'))
+})
+const saveNavState = (activePath: string) => {
+  window.sessionStorage.setItem('activePath', activePath)
+  activeP.value = activePath
+}
+</script>
+
+<style scoped>
+</style>
